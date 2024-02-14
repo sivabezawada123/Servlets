b4bca4bfb2196de7ae4a15d2a5f5513353dddbe2
ShivaKumar <sivakumarbezawada11@gmail.com>
2024-02-13 18:13:03 -0600

    commit

---
+++ src/main/java/com/java/cis/LogOutServlet.java
@@ -0,0 +1,16 @@
+package com.java.cis;
+
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+public class LogOutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("login.html");
+    }
+}
